d6e4a4e5ab72bc88fb1572b44912fb3494de8c63
Attila <jooattila@hotmail.com>
2021-12-28 18:43:53 +0200

    Forks task done
    No design and there is a button for getting forks. Need to correct it.

--- src/components/Forks/Forks.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Forks = (props: any) => {
-  return <div></div>;
-};
-
-export default Forks;
--- src/components/Gist/Gist.tsx
+++ src/components/Gist/Gist.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FileTag from '../FileTag/FileTag';
 import './index.css';
 
 const Gist = (props: any) => {
   const languages = Object.values(props.data.files).map((file: any) => file.language);
-  const unique = [...new Set<any>(languages)];
+  const uniqueLanguages = [...new Set<any>(languages)];
+  const forksUrl = props.data.forks_url;
+  const [forks, setForks] = useState([]);
+
+  const getForks = async () => {
+    try {
+      setForks(await fetch(forksUrl).then(res => res.json()));
+      forks.forEach((element: { created_at: any }) => {
+        element.created_at = Date.parse(element.created_at);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const sorted = forks.sort((a: any, b: any) => (a.created_at < b.created_at ? 1 : -1)).slice(0, 3);
 
   return (
     <div className='gistDiv'>
@@ -13,9 +28,15 @@ const Gist = (props: any) => {
       <p>Created at: {props.data.created_at}</p>
       <a href={props.data.forks_url}>Forks url: {props.data.forks_url}</a>
       <p className='languageLabel'>Languages: </p>
-      {unique.map(language => {
+      {uniqueLanguages.map(language => {
         return <FileTag language={language} key={language} />;
       })}
+      <button onClick={getForks}>Get Forks</button>
+      {sorted &&
+        sorted.map((fork: any) => {
+          console.log(fork.owner.login);
+          return <h3 key={fork.owner.login}>username: {fork.owner.login}</h3>;
+        })}
     </div>
   );
 };
